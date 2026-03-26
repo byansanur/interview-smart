3e363c3adec8218a1ba01ff8d7607af108d11ec1
ratbyansa nur <49193857+byansanur@users.noreply.github.com>
2026-03-26 08:02:19 +0000

    doc: update comment on app.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ export default function App() {
   const [result, setResult] = useState(0);
 
   const handleChange = (e) => {
+    // only allow the positif number
     const sanitizedValue = e.target.value.replace(/\D/g, "");
     setInputValue(sanitizedValue);
   };
 
   function handleInput() {
     //please put your logic here
+
+    // handling empty input
     if (!inputValue) {
       setResult(0);
       return;
     }
 
     const originalNum = parseInt(inputValue, 10);
+
+    // Reverse the order of numbers and make them integers (automatically remove leading zeros)
     const reversedNum = parseInt(inputValue.split("").reverse().join(""), 10);
-    console.log(reversedNum);
+
+    // calculate the absolut different (result is always positive.)
     const difference = Math.abs(originalNum - reversedNum);
     setResult(difference);
   }
